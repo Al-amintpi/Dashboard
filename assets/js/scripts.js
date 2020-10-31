bf2d0041e5fc7f5a73c9d5acc474e069047e158e
Al-amintpi <alamincmt7418@gmail.com>
2020-11-01 00:01:28 +0600

    Test parpase

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -6,4 +6,34 @@ $(document).ready(function(){
         effectsIn: 'fade translateY(-100%)'
     }
 	});
+
+	$('.menu-icon').click(function(){
+		$('.left-content-slidebar').show();
+		return false;
+	});
+
+	$('.navbar-hide-show').click(function(){
+		$('.left-content-slidebar').hide();
+		return false;
+		 
+	});
+
+	$('.search-button').click(function(){
+		$('.search-bar').show();
+		return false;
+	});
+	
+	$('.search-close').click(function(){
+		$('.search-bar').hide();
+		return false;
+	});
 });
+
+$(document).ready(function() {
+  $('.popup-button').magnificPopup({
+		  type:'inline',
+		  midClick: true 
+	});
+});
+
+
